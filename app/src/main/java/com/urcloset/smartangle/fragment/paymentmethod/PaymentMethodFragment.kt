3eb248715e8cdb88bdb84259c373b3942f2d1d15
AbdelrahmanSalahGabe <abdelrahman.salah@grinta.ai>
2024-12-28 17:27:31 +0200

    update payment flow and add product

--- app/src/main/java/com/urcloset/smartangle/fragment/paymentmethod/PaymentMethodFragment.kt
+++ app/src/main/java/com/urcloset/smartangle/fragment/paymentmethod/PaymentMethodFragment.kt
@@ -1,10 +1,10 @@
 package com.urcloset.smartangle.fragment.paymentmethod
 
+import android.app.Activity.RESULT_OK
 import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,18 +13,14 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.google.gson.Gson
 import com.myfatoorah.sdk.entity.executepayment.MFExecutePaymentRequest
-import com.myfatoorah.sdk.entity.initiatepayment.MFInitiatePaymentRequest
-import com.myfatoorah.sdk.entity.initiatepayment.MFInitiatePaymentResponse
 import com.myfatoorah.sdk.entity.paymentstatus.MFGetPaymentStatusResponse
 import com.myfatoorah.sdk.utils.MFAPILanguage
 import com.myfatoorah.sdk.utils.MFCountry
-import com.myfatoorah.sdk.utils.MFCurrencyISO
 import com.myfatoorah.sdk.utils.MFEnvironment
 import com.myfatoorah.sdk.views.MFResult
 import com.myfatoorah.sdk.views.MFSDK
 import com.urcloset.smartangle.R
 import com.urcloset.smartangle.activity.homeActivity.HomeActivity
-import com.urcloset.smartangle.activity.homeActivity.HomeViewModel
 import com.urcloset.smartangle.databinding.FragmentPaymentMethodBinding
 import com.urcloset.smartangle.tools.BasicTools
 import com.urcloset.smartangle.tools.GetObjectGson
@@ -35,16 +31,18 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class PaymentMethodFragment : TemplateFragment() {
-    lateinit var binding : FragmentPaymentMethodBinding
-    val viewModel :PaymentViewModel by viewModels()
-    @Inject lateinit var getGsonObject  :GetObjectGson
-    @Inject lateinit var progressDialog : Dialog
-    var selectedPayment : HashMap<String,Any>?= null
+    lateinit var binding: FragmentPaymentMethodBinding
+    val viewModel: PaymentViewModel by viewModels()
+    @Inject
+    lateinit var getGsonObject: GetObjectGson
+    @Inject
+    lateinit var progressDialog: Dialog
+    var selectedPayment: HashMap<String, Any>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MFSDK.init(TOKENLIVE, MFCountry.SAUDI_ARABIA, MFEnvironment.LIVE)
-        BasicTools.setLanguagePerActivity(requireActivity(),null)
-      selectedPayment = getGsonObject.getGenericMap(arguments?.getString(PAYMENTPUBLISH)?:"")
+        MFSDK.init(TOKENLIVE/*TESTTOKEN*/, MFCountry.SAUDI_ARABIA, MFEnvironment.LIVE)
+        BasicTools.setLanguagePerActivity(requireActivity(), null)
+        selectedPayment = getGsonObject.getGenericMap(arguments?.getString(PAYMENTPUBLISH) ?: "")
     }
 
     override fun init_events() {
@@ -70,8 +68,10 @@ class PaymentMethodFragment : TemplateFragment() {
                 is MFResult.Success -> {
                     binding.mfPaymentView.load(it.response)
                 }
+
                 is MFResult.Fail -> {
-                    Toast.makeText(requireContext(), Gson().toJson(it.error),Toast.LENGTH_SHORT).show()
+                    Toast.makeText(requireContext(), Gson().toJson(it.error), Toast.LENGTH_SHORT)
+                        .show()
                 }
 
                 else -> {}
@@ -79,34 +79,37 @@ class PaymentMethodFragment : TemplateFragment() {
             binding.progressLoader.visibility = View.GONE
 
         }
-        binding.payNow.setOnClickListener{
-            payButton((selectedPayment?.get(AMOUNT)?:"0.0").toString().toDouble())
-          /*  viewModel.postPaymentAfterPay(selectedPayment.also {
-                it?.put(PAYMENTID, 1902501)
-                it?.put(PAYMENT_STATUS, "success")
-                it?.put(RESPONSEOBJECT,"{\"CreatedDate\":\"2022-12-11T19:02:43.17\",\"CustomerMobile\":\"+965\",\"CustomerName\":\"badr\",\"ExpiryDate\":\"December 14, 2022\",\"InvoiceDisplayValue\":\"10.000 KD\",\"InvoiceId\":1902501,\"InvoiceItems\":[],\"InvoiceReference\":\"2022341740\",\"InvoiceStatus\":\"Paid\",\"InvoiceTransactions\":[{\"AuthorizationId\":\"024176\",\"Currency\":\"KD\",\"CustomerServiceCharge\":\"0.000\",\"DueValue\":\"10.000\",\"PaidCurrency\":\"KD\",\"PaidCurrencyValue\":\"10.000\",\"PaymentGateway\":\"VISA/MASTER\",\"PaymentId\":\"07071902501137592271\",\"ReferenceId\":\"234516298477\",\"TrackId\":\"11-12-2022_1375922\",\"TransactionDate\":\"2022-12-11T19:02:54.22\",\"TransactionId\":\"298477\",\"TransactionStatus\":\"Succss\",\"TransationValue\":\"10.000\"}],\"InvoiceValue\":10.0,\"Suppliers\":[]}")
-                it?.put(INVOICEID,1902501)
-            })*/
+        binding.payNow.setOnClickListener {
+            payButton((selectedPayment?.get(AMOUNT) ?: "0.0").toString().toDouble())
+            /*  viewModel.postPaymentAfterPay(selectedPayment.also {
+                  it?.put(PAYMENTID, 1902501)
+                  it?.put(PAYMENT_STATUS, "success")
+                  it?.put(RESPONSEOBJECT,"{\"CreatedDate\":\"2022-12-11T19:02:43.17\",\"CustomerMobile\":\"+965\",\"CustomerName\":\"badr\",\"ExpiryDate\":\"December 14, 2022\",\"InvoiceDisplayValue\":\"10.000 KD\",\"InvoiceId\":1902501,\"InvoiceItems\":[],\"InvoiceReference\":\"2022341740\",\"InvoiceStatus\":\"Paid\",\"InvoiceTransactions\":[{\"AuthorizationId\":\"024176\",\"Currency\":\"KD\",\"CustomerServiceCharge\":\"0.000\",\"DueValue\":\"10.000\",\"PaidCurrency\":\"KD\",\"PaidCurrencyValue\":\"10.000\",\"PaymentGateway\":\"VISA/MASTER\",\"PaymentId\":\"07071902501137592271\",\"ReferenceId\":\"234516298477\",\"TrackId\":\"11-12-2022_1375922\",\"TransactionDate\":\"2022-12-11T19:02:54.22\",\"TransactionId\":\"298477\",\"TransactionStatus\":\"Succss\",\"TransationValue\":\"10.000\"}],\"InvoiceValue\":10.0,\"Suppliers\":[]}")
+                  it?.put(INVOICEID,1902501)
+              })*/
         }
-     /*   val request = MFInitiatePaymentRequest(0.100, MFCurrencyISO.SAUDI_ARABIA_SAR)
-        MFSDK.initiatePayment(
-            request,
-           BasicTools.getLangCode(requireContext())?:"en"
-        ) { result: MFResult<MFInitiatePaymentResponse> ->
-            when (result) {
-                is MFResult.Success ->
-                    binding.mfPaymentView.load(result.response )
+        /*   val request = MFInitiatePaymentRequest(0.100, MFCurrencyISO.SAUDI_ARABIA_SAR)
+           MFSDK.initiatePayment(
+               request,
+              BasicTools.getLangCode(requireContext())?:"en"
+           ) { result: MFResult<MFInitiatePaymentResponse> ->
+               when (result) {
+                   is MFResult.Success ->
+                       binding.mfPaymentView.load(result.response )
 
-                is MFResult.Fail ->
-                    Log.d("TAG", "Fail: " + Gson().toJson(result.error))
-            }
-        }*/
+                   is MFResult.Fail ->
+                       Log.d("TAG", "Fail: " + Gson().toJson(result.error))
+               }
+           }*/
         setViewModelObservers()
         return binding.root
     }
-    fun payButton(amount : Double){
+
+    fun payButton(amount: Double) {
         val request = MFExecutePaymentRequest(amount)
-        request.customerName =BasicTools.getUserName(requireContext()) + " id  :" + (loginResponse?.data?.user?.id?:0).toString()
+        request.customerName =
+            BasicTools.getUserName(requireContext()) //+ " id  :" + (loginResponse?.data?.user?.id
+               // ?: 0).toString()
         binding.mfPaymentView.pay(
             requireActivity(),
             request,
@@ -117,26 +120,28 @@ class PaymentMethodFragment : TemplateFragment() {
         ) { invoiceId: String, result: MFResult<MFGetPaymentStatusResponse> ->
             when (result) {
                 is MFResult.Success -> {
-                    result.response.let {resultPay->
+                    result.response.let { resultPay ->
                         viewModel.postPaymentAfterPay(selectedPayment.also {
-                            it?.put(PAYMENTID, resultPay.invoiceId?:0)
+                            it?.put(PAYMENTID, resultPay.invoiceId ?: 0)
                             it?.put(PAYMENT_STATUS, "success")
-                            it?.put(RESPONSEOBJECT,Gson().toJson(resultPay))
-                            it?.put(INVOICEID,resultPay.invoiceId?:0)
+                            it?.put(RESPONSEOBJECT, Gson().toJson(resultPay))
+                            it?.put(INVOICEID, resultPay.invoiceId ?: 0)
                         })
                     }
                     Log.d("TAG", "Response: " + Gson().toJson(result.response))
                 }
-                is MFResult.Fail -> {
-                    Toast.makeText(requireContext(),result.error.message?:"",Toast.LENGTH_SHORT).show()
-                 /*   viewModel.postPaymentAfterPay(selectedPayment.also {
-                        it?.put(PAYMENTID, invoiceId?:0)
-                        it?.put(PAYMENT_STATUS, "error")
-                        it?.put(RESPONSEOBJECT,result)
-                        it?.put(INVOICEID,invoiceId?:0)
-                        it?.put(ERRORMESSAGE,result.error.message?:"")
 
-                    })*/
+                is MFResult.Fail -> {
+                    Toast.makeText(requireContext(), result.error.message ?: "", Toast.LENGTH_SHORT)
+                        .show()
+                    /*   viewModel.postPaymentAfterPay(selectedPayment.also {
+                           it?.put(PAYMENTID, invoiceId?:0)
+                           it?.put(PAYMENT_STATUS, "error")
+                           it?.put(RESPONSEOBJECT,result)
+                           it?.put(INVOICEID,invoiceId?:0)
+                           it?.put(ERRORMESSAGE,result.error.message?:"")
+
+                       })*/
                     Log.d("TAG", "Fail: " + Gson().toJson(result.error))
                 }
 
@@ -145,18 +150,27 @@ class PaymentMethodFragment : TemplateFragment() {
             Log.d("TAG", "invoiceId: $invoiceId")
         }
     }
+
     fun setViewModelObservers() {
-        viewModel.postPayment.observe(viewLifecycleOwner, Observer{ // for sending to maintenance
-            if (it!=null){
-                Toast.makeText(requireContext(),getString(R.string.process_completed),Toast.LENGTH_SHORT).show()
-                requireActivity().startActivity(Intent(requireContext(),HomeActivity::class.java))
+        viewModel.postPayment.observe(viewLifecycleOwner, Observer { // for sending to maintenance
+            if (it != null) {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.process_completed),
+                    Toast.LENGTH_SHORT
+                ).show()
+                requireActivity().startActivity(Intent(requireContext(), HomeActivity::class.java))
                 requireActivity().finish()
+               /* val data = Intent()
+                data.putExtra("sda", "success") // this paths is from storage to what inside
+                (requireActivity()).setResult(RESULT_OK, data)
+                requireActivity().finish() // this*/
             }
 
         })
 
-        viewModel.networkLoader.observe(viewLifecycleOwner, Observer{
-            it?.let { progress->
+        viewModel.networkLoader.observe(viewLifecycleOwner, Observer {
+            it?.let { progress ->
                 progress.setDialog(progressDialog) // open close principles
                 viewModel.setNetworkLoader(null)
             }
@@ -173,20 +187,25 @@ class PaymentMethodFragment : TemplateFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
+
     companion object {
-        const val API_KEY = "rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL"
-        const val TOKENLIVE = "RgGTCq_zjK00pbggFDqYwYZbegxus6hlMkVtiE_sN9hNPsBp3YV3s9r3fiLII2e7tbSm9dOgzpr9_aQ2SFfSrvLj9J0HI2wBZtAdwnsesdU079FEdr9kaQVqlxcKo84ACqkBWov6Ba7K_g8KD93vB0EZ7bxo2DlB9_kGkR9uVLRrc77GxNGLjY8ySw_MHPNxmT2q2WCMdnmIh5AZue-L81iQ-xSQnUBpuLFk7IMMtsZ0wsF9mDOOGtmL7obWC2mKgKxh2Z3WYt2ygaKc7OmxbOCL0C6C7mfLDZhjJHsoCYAU18VtOoEb_J7ETYIVDfSEeJHVTHros_dq56-rM68TTvWmdog1VMdPcV3bmrg9GIXnMplzE_oWTF8Y-T96L1q8YSvoUYoewvx9U7Bt9FW1fGeaSVpxN4pSa-i19rEv_odHb7IreJsdPWNpp5ifsPV0JFgV92qB3peTDmXvdh7jtIxWrWfo0-FolqowvD3XKpfNHxWSsSDIaBKql8LrkWdUrEd1jPN_gEwpFblLcaxwb2x_aziPfZOwjJizbOSRUVu6Boaq68i2zVHaaL-zwyZ1kDhkdh1BlYAGIjNqo1XiPCk9GY3noGIzWygREMRfiEJKNy0ndTcXaQ_EYGVXuTHzW1i9mvsVYBlEhXyM28lsaGlB_is4J6iNnh075e1T-eI8Ca4FCRFuMrequll2Pmibd_rL8VZWRBN34_NblVWluhdiiH9HHJLEFORI2gx5CpXIaA_t"
-        const val TESTTOKEN = "rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL"
-        const val TESTTOKENII = "rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL"
+        const val API_KEY =
+            "rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL"
+        const val TOKENLIVE =
+            "RgGTCq_zjK00pbggFDqYwYZbegxus6hlMkVtiE_sN9hNPsBp3YV3s9r3fiLII2e7tbSm9dOgzpr9_aQ2SFfSrvLj9J0HI2wBZtAdwnsesdU079FEdr9kaQVqlxcKo84ACqkBWov6Ba7K_g8KD93vB0EZ7bxo2DlB9_kGkR9uVLRrc77GxNGLjY8ySw_MHPNxmT2q2WCMdnmIh5AZue-L81iQ-xSQnUBpuLFk7IMMtsZ0wsF9mDOOGtmL7obWC2mKgKxh2Z3WYt2ygaKc7OmxbOCL0C6C7mfLDZhjJHsoCYAU18VtOoEb_J7ETYIVDfSEeJHVTHros_dq56-rM68TTvWmdog1VMdPcV3bmrg9GIXnMplzE_oWTF8Y-T96L1q8YSvoUYoewvx9U7Bt9FW1fGeaSVpxN4pSa-i19rEv_odHb7IreJsdPWNpp5ifsPV0JFgV92qB3peTDmXvdh7jtIxWrWfo0-FolqowvD3XKpfNHxWSsSDIaBKql8LrkWdUrEd1jPN_gEwpFblLcaxwb2x_aziPfZOwjJizbOSRUVu6Boaq68i2zVHaaL-zwyZ1kDhkdh1BlYAGIjNqo1XiPCk9GY3noGIzWygREMRfiEJKNy0ndTcXaQ_EYGVXuTHzW1i9mvsVYBlEhXyM28lsaGlB_is4J6iNnh075e1T-eI8Ca4FCRFuMrequll2Pmibd_rL8VZWRBN34_NblVWluhdiiH9HHJLEFORI2gx5CpXIaA_t"
+        const val TESTTOKEN =
+            "rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL"
+        const val TESTTOKENII =
+            "rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL"
         val IDS = "ids"
         val SELECTED_PRODUCTS = "SELECTED_PRODUCTS"
-     val PAYMENTID = "payment_id"
+        val PAYMENTID = "payment_id"
         const val SOLD_IDS = "sold_ids"
-     val PAYMENT_STATUS = "payment_status"
-     val AMOUNT = "amount"
-     val RESPONSEOBJECT = "response_object"
-     val INVOICEID = "invoice_id"
-     val ERRORMESSAGE = "error_message"
-     val PAYMENTPUBLISH  = "PAYMENT_PUBLISH"
+        val PAYMENT_STATUS = "payment_status"
+        val AMOUNT = "amount"
+        val RESPONSEOBJECT = "response_object"
+        val INVOICEID = "invoice_id"
+        val ERRORMESSAGE = "error_message"
+        val PAYMENTPUBLISH = "PAYMENT_PUBLISH"
     }
 }
